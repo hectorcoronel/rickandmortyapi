8ee56433f99591bc91ad8475096af8e20d077f54
hectorcoronel <hectorg.coronel@gmail.com>
2020-11-24 13:44:36 -0800

    se creo el home y characters

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output:{
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
-    },
-    resolve:{
-        extension: ['.js'],
-    },
-    module:{
-        rules:[
-            {
-                test: /\.js?$/,
-                exclude: /node_modeles/,
-                use: {
-                    loader: 'babel-loader',
-                }
-            }
-        ]
-    },
-    plugin:[
-        new HtmlWebpackPlugin([
-            {
-                inyect: true,
-                template: './public/index.html',
-                filename: './index.html'
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         }
-    ])
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin([
+      {
+        inject: true,
+        template: './public/index.html',
+        filename: './index.html',
+      }
+    ]),
+  ]
 }
